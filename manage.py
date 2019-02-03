bd60c31cc835b67f892af8f690defa46d7b5af87
Theodore Keloglou <theodorekeloglou@gmail.com>
2019-02-03 18:03:27 +0200

    add dotenv to manage file

--- manage.py
+++ manage.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python
 import os
 import sys
+from os.path import dirname, join
 
-if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'oscarator.settings')
+from dotenv import load_dotenv
+
+if __name__ == "__main__":
+    dotenv_path = join(dirname(__file__), ".env")
+    load_dotenv(dotenv_path)
+
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "oscarator.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
