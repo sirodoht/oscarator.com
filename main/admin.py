6ab7896b834f2f8b014dfcc291a244e81e50739c
Theodore Keloglou <theodorekeloglou@gmail.com>
2019-02-19 14:52:56 +0200

    add fields on admin models

--- main/admin.py
+++ main/admin.py
@@ -1,9 +1,34 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
 from main import models
 
-admin.site.register(models.Profile)
-admin.site.register(models.Analytic)
+
+# User
+class OscaratorAdmin(UserAdmin):
+    list_display = ("username", "email", "date_joined", "last_login", "id")
+
+
+admin.site.unregister(User)
+admin.site.register(User, OscaratorAdmin)
+
+
+# Profile
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ("user", "id")
+
+
+admin.site.register(models.Profile, ProfileAdmin)
+
+
+# Analytic
+class AnalyticAdmin(admin.ModelAdmin):
+    list_display = ("ip", "user", "created_at", "path", "querystring", "id")
+
+
+admin.site.register(models.Analytic, AnalyticAdmin)
+
 admin.site.register(models.Category)
 admin.site.register(models.Entry)
 admin.site.register(models.Vote)
