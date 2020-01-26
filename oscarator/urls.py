30b95f99d051726b51782f930d250a52a0832cbe
Theodore Keloglou <zf@sirodoht.com>
2020-01-26 02:51:41 +0000

    add favicon

--- oscarator/urls.py
+++ oscarator/urls.py
@@ -15,5 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import RedirectView
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include("main.urls"))]
+from oscarator import settings
+
+urlpatterns = [
+    path("favicon.ico", RedirectView.as_view(url=settings.STATIC_URL + "favicon.ico")),
+    path("admin/", admin.site.urls),
+    path("", include("main.urls")),
+]
