bf63e10a9caf7612e194e94b89b5a42bd91824ed
Theodore Keloglou <zf@sirodoht.com>
2022-02-16 22:56:31 +0000

    update manage.py to latest version

--- manage.py
+++ manage.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
 import os
 import sys
-from os.path import dirname, join
 
-from dotenv import load_dotenv
 
-if __name__ == "__main__":
-    dotenv_path = join(dirname(__file__), ".env")
-    load_dotenv(dotenv_path)
-
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "oscarator.settings")
+def main():
+    """Run administrative tasks."""
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chaitin.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -19,3 +16,7 @@ if __name__ == "__main__":
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
+
+
+if __name__ == "__main__":
+    main()
