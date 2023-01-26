d7a2d231f2f492a607a692631ea5b9e6af6d2a5b
Theodore Keloglou <zf@sirodoht.com>
2023-01-26 23:20:36 +0000

    fix smtp host

--- oscarator/settings.py
+++ oscarator/settings.py
@@ -144,7 +144,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_USE_TLS = True
-EMAIL_HOST = "email-smtp.eu-west-1.amazonaws.com"
+EMAIL_HOST = "smtp.postmarkapp.com"
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 EMAIL_PORT = 587
